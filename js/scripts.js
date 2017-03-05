368d8f65210ab6a1193921d4ebb9f88f5eb85583
Xi Xia <triplextension@gmail.com>
2017-03-04 23:27:05 -0800

    add selector and checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ function Pizza(sizeOf,crust,sauce,cheese,veggieToppings,meatToppings,cost){
   this.crust=crust;
   this.sauce=sauce;
   this.cheese=cheese,
-  this.veggieToppings=[];
-  this.meatToppings=[];
+  this.veggieToppings=veggieToppings;
+  this.meatToppings=meatToppings;
   this.cost=8;
 }
 
@@ -17,27 +17,35 @@ function Order(pizzas,totalCost,deliveryAddress){
 }
 
 Pizza.prototype.howMuch = function(){
-  var pizzaCost;
   //cost adjustment on Size of pizza
-  pizzaCost =(this.sizeOf==="large") ? (this.cost=(4+this.cost)):(this.cost);
-  pizzaCost =(this.sizeOf==="Xlarge") ? (this.cost=(7+this.cost)):(this.cost);
+  this.cost=8;
+  (this.sizeOf==="large") ? (this.cost=(4+this.cost)):(this.cost);
+  (this.sizeOf==="Xlarge") ? (this.cost=(7+this.cost)):(this.cost);
   //cost adjustment on Crust of pizza
-   pizzaCost = (this.crust==="thin") ? (this.cost=(1+pizzaCost)):(this.cost=pizzaCost);
-   pizzaCost = (this.crust==="deepdish") ? (this.cost=(2+pizzaCost)):(this.cost=pizzaCost);
+  (this.crust==="thin") ? (this.cost=(1+this.cost)):(this.cost);
+  (this.crust==="deepdish") ? (this.cost=(2+this.cost)):(this.cost);
   //cost adjustment on sauce
-   pizzaCost = (this.sauce==="oil") ? (this.cost=(1+pizzaCost)):(this.cost=pizzaCost);
-   pizzaCost = (this.sauce==="pesto") ? (this.cost=(2+pizzaCost)):(this.cost=pizzaCost);
+  (this.sauce==="oil") ? (this.cost=(1+this.cost)):(this.cost);
+  (this.sauce==="pesto") ? (this.cost=(2+this.cost)):(this.cost);
   //cost adjustment on cheese
-   pizzaCost = (this.cheese==="extra") ? (this.cost=(1+pizzaCost)):(this.cost=pizzaCost);
-   pizzaCost = (this.cheese==="blend") ? (this.cost=(2+pizzaCost)):(this.cost=pizzaCost);
+  (this.cheese==="extra") ? (this.cost=(1+this.cost)):(this.cost);
+  (this.cheese==="blend") ? (this.cost=(2+this.cost)):(this.cost);
   //cost adjustment on veggie toppings
-  this.veggieToppings.forEach(function(topping){
-     (/arugula|garlic|basil|olives|pepperoncini|pineapple|mushrooms|/.test(topping)) ? (pizzaCost=(this.cost=(1+pizzaCost))):(this.cost=pizzaCost);
-  });
+  var  pizzaCost=this.cost;
+  if (this.veggieToppings !== undefined){
+    this.veggieToppings.forEach(function(topping){
+       (/arugula|garlic|basil|olives|pepperoncini|pineapple|mushrooms|/.test(topping)) ? (pizzaCost=(1+pizzaCost)):(pizzaCost);
+    });
+  }
   //cost adjustment on meat toppings
-  this.meatToppings.forEach(function(topping){
-     (/pepperoni|bacon|sausage|salami|capicollo|anchovies/.test(topping)) ? (pizzaCost=(this.cost=(2+pizzaCost))):(this.cost=pizzaCost);
-  });
+  console.log(this.meatToppings);
+  if (this.meatToppings !== undefined){
+    this.meatToppings.forEach(function(topping){
+       (/pepperoni|bacon|sausage|salami|capicollo|anchovies/.test(topping)) ? (pizzaCost=(2+pizzaCost)):(pizzaCost);
+    });
+  }
+  this.cost=pizzaCost;
+  return this.cost;
 }
 Order.prototype.place = function(){
   var totalCost=0;
@@ -47,12 +55,29 @@ Order.prototype.place = function(){
   this.totalCost = totalCost;
 }
 
-var pizza1=new Pizza ();
-var pizza2=new Pizza ();
 
 
 //user logic
 $(function(){
-
-
+  var meatToppingsList =[];
+  var pizzaInOrder=[];
+  var newPizza = new Pizza();
+//create pizza object
+  $("#startPizza").click(function(){
+    $("#pizzaForm").show();
+  });
+//pizza size, change price
+  $("#pizzaSize").change(function(){
+    newPizza.sizeOf=$("#pizzaSize").val();
+    $("#pizzaPrice").empty().append("<h1> Pizza Cost: $"+newPizza.howMuch()+"</h1>");
+  });
+//meat toppings, increase and decrease price.
+  $("#pizzaMeatToppings input[type=checkbox]").change(function(){
+    newPizza.meatToppings=[];
+    $("#meatToppingsDisplay").empty();
+    $("#pizzaMeatToppings input[type=checkbox]:checked").each(function(){
+      newPizza.meatToppings.push($(this).val());
+    });
+    $("#pizzaPrice").empty().append("<h1> Pizza Cost: $"+newPizza.howMuch()+"</h1>");
+  });
 });
