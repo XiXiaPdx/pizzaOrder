36b58127496f894af73bd95522ae3fca428c971c
Xi Xia <triplextension@gmail.com>
2017-03-05 00:39:00 -0800

    add order total, pizza order price

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 //business logic
 // objects - pizza, order
-function Pizza(sizeOf,crust,sauce,cheese,veggieToppings,meatToppings,cost){
+function Pizza(sizeOf,crust,veggieToppings,meatToppings,cost){
   this.sizeOf=sizeOf;
   this.crust=crust;
-  this.sauce=sauce;
-  this.cheese=cheese,
   this.veggieToppings=veggieToppings;
   this.meatToppings=meatToppings;
   this.cost=8;
 }
 
 function Order(pizzas,totalCost,deliveryAddress){
-  this.pizzas=pizzas;
-  this.totalCost=0;
+  this.pizzas=[];
+  this.totalCost=totalCost;
   this.deliveryAddress=deliveryAddress;
 }
 
@@ -24,12 +22,6 @@ Pizza.prototype.howMuch = function(){
   //cost adjustment on Crust of pizza
   (this.crust==="thin") ? (this.cost=(1+this.cost)):(this.cost);
   (this.crust==="deepdish") ? (this.cost=(2+this.cost)):(this.cost);
-  //cost adjustment on sauce
-  (this.sauce==="oil") ? (this.cost=(1+this.cost)):(this.cost);
-  (this.sauce==="pesto") ? (this.cost=(2+this.cost)):(this.cost);
-  //cost adjustment on cheese
-  (this.cheese==="extra") ? (this.cost=(1+this.cost)):(this.cost);
-  (this.cheese==="blend") ? (this.cost=(2+this.cost)):(this.cost);
   //cost adjustment on veggie toppings
   var  pizzaCost=this.cost;
   if (this.veggieToppings !== undefined){
@@ -38,7 +30,6 @@ Pizza.prototype.howMuch = function(){
     });
   }
   //cost adjustment on meat toppings
-  console.log(this.meatToppings);
   if (this.meatToppings !== undefined){
     this.meatToppings.forEach(function(topping){
        (/pepperoni|bacon|sausage|salami|capicollo|anchovies/.test(topping)) ? (pizzaCost=(2+pizzaCost)):(pizzaCost);
@@ -47,37 +38,64 @@ Pizza.prototype.howMuch = function(){
   this.cost=pizzaCost;
   return this.cost;
 }
-Order.prototype.place = function(){
+Order.prototype.howMuch = function(){
   var totalCost=0;
   this.pizzas.forEach(function(pizza,index){
     totalCost=pizza.cost+totalCost;
   });
-  this.totalCost = totalCost;
+  return this.totalCost = totalCost;
 }
 
 
 
 //user logic
 $(function(){
-  var meatToppingsList =[];
-  var pizzaInOrder=[];
   var newPizza = new Pizza();
+  var yourOrder=new Order();
 //create pizza object
   $("#startPizza").click(function(){
     $("#pizzaForm").show();
+    $("#startPizza").toggle();
   });
 //pizza size, change price
   $("#pizzaSize").change(function(){
     newPizza.sizeOf=$("#pizzaSize").val();
     $("#pizzaPrice").empty().append("<h1> Pizza Cost: $"+newPizza.howMuch()+"</h1>");
   });
+//pizza crust
+  $("#pizzaCrust").change(function(){
+    newPizza.crust=$("#pizzaCrust").val();
+    $("#pizzaPrice").empty().append("<h1> Pizza Cost: $"+newPizza.howMuch()+"</h1>");
+  });
 //meat toppings, increase and decrease price.
   $("#pizzaMeatToppings input[type=checkbox]").change(function(){
     newPizza.meatToppings=[];
-    $("#meatToppingsDisplay").empty();
     $("#pizzaMeatToppings input[type=checkbox]:checked").each(function(){
       newPizza.meatToppings.push($(this).val());
     });
     $("#pizzaPrice").empty().append("<h1> Pizza Cost: $"+newPizza.howMuch()+"</h1>");
+  });
+//veggie toppings, increase and decrease price.
+    $("#pizzaVeggieToppings input[type=checkbox]").change(function(){
+      newPizza.veggieToppings=[];
+      $("#pizzaVeggieToppings input[type=checkbox]:checked").each(function(){
+        newPizza.veggieToppings.push($(this).val());
+      });
+      $("#pizzaPrice").empty().append("<h1> Pizza Cost: $"+newPizza.howMuch()+"</h1>");
+    });
+//add to order
+  $("#pizzaForm").submit(function(event){
+    event.preventDefault();
+    yourOrder.pizzas.push(newPizza);
+    $("#firstPizza").hide();
+    $("#morePizza").show();
+    $("#pizzaPrice").empty();
+    $("#pizzaForm").trigger("reset");
+    $("#orderCart h1").append("<h3>"+"Pizza #"+(yourOrder.pizzas.length+"</h3>"));
+    $("#orderPrice").empty().prepend("<h2> Order Total: $"+yourOrder.howMuch()+"</h2>");
+    newPizza= new Pizza();
+    console.log(yourOrder);
+
+
   });
 });
